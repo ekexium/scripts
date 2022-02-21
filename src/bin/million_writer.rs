0c3cef2a57dd9d00d765dec7ad3d281d73f61ece
ekexium <ekexium@gmail.com>
2022-02-21 18:05:32 +0800

    speedup
    Signed-off-by: ekexium <ekexium@gmail.com>

--- src/bin/million_writer.rs
+++ src/bin/million_writer.rs
@@ -2,18 +2,19 @@
 // the i-th row: <i 2*i>
 
 use dmlddl::Result;
-use futures::stream;
-use futures::StreamExt;
 use log::LevelFilter;
 use sqlx::mysql::MySqlPoolOptions;
 use sqlx::Executor;
+use std::sync::atomic::AtomicU32;
 use std::sync::Arc;
+
+const COUNTER: AtomicU32 = AtomicU32::new(0);
 
 #[tokio::main]
 async fn main() -> Result<()> {
     simple_logging::log_to_file("dmlddl.log", LevelFilter::Info)?;
     let pool = MySqlPoolOptions::new()
-        .max_connections(32)
+        .max_connections(500)
         .connect("mysql://root@127.0.0.1:4000/test")
         .await?;
     let pool = Arc::new(pool);
@@ -21,20 +22,34 @@ async fn main() -> Result<()> {
     let mut conn = pool.acquire().await?;
     conn.execute("use test").await?;
     conn.execute("drop table if exists test").await?;
-    conn.execute("create table t(a int, b int)").await?;
+    conn.execute("create table t(a int primary key, b int)")
+        .await?;
     drop(conn);
 
-    let keys = 0..10_0;
-    stream::iter(keys)
-        .for_each_concurrent(32, |i| {
-            let pool = pool.clone();
-            async move {
-                pool.clone()
-                    .execute(format!("insert into t values({}, {})", i, 2 * i).as_str())
-                    .await
-                    .expect("insert fail");
+    let mut handles = Vec::new();
+    let max = 10_000;
+    for _ in 0..32 {
+        let mut conn = pool.acquire().await?;
+        handles.push(tokio::spawn(async move {
+            let x = COUNTER.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+            if x >= max {
+                return;
             }
-        })
-        .await;
+            conn.execute("begin").await.expect("begin failed");
+            for i in 0..1000 {
+                conn.execute(
+                    format!(
+                        "insert into t values({}, {})",
+                        x * 1000 + i,
+                        (x * 1000 + i) * 2
+                    )
+                    .as_str(),
+                )
+                .await
+                .expect("insert failed");
+            }
+            conn.execute("commit").await.expect("commit failed");
+        }));
+    }
     Ok(())
 }
